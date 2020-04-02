087c2ca31b9d90264bbadd4186b1c65b97b7bf41
szypkiwonsz <kacpersawicki321@gmail.com>
2020-04-02 16:38:02 +0200

    Code improvement

--- generator.py
+++ generator.py
@@ -4,171 +4,154 @@ import sys
 class HTMLGenerator:
     def __init__(self, text):
         self.text = text
-        self.word = ""
-        self.output = ""
-        self.line = ""
-        self.final_output = ""
-        self.line = ""
+        self.lines = None
+        self.output = ''
+        self.href_check = True
 
-    def replace(self):
-        self.word = self.word.replace(self.word[0], "")
-        self.word = self.word.replace(self.word[-1], "")
+    def split_for_lines(self, data):
+        self.lines = data.split('\n')
 
-    def sharp_parentheses(self):
-        if self.check_heading():
-            self.word = "#<q>{}</q>".format(self.word[3:-2])
-        else:
-            self.word = "<q>{}</q>".format(self.word[2:-2])
+    def check_for_headings_in_lines(self):
+        lines = ''
+        id_heading = 0
+        for line in self.lines:
+            if line[0] == '#':
+                line = f'<h1 id="{id_heading}X"> {line[1:]} </h1>'
+                id_heading += 1
+            lines += line + '\n'
+        self.lines = lines
+        self.split_for_lines(self.lines[:-1])
 
-    def one_star(self):
-        if self.check_heading():
-            self.word = "#<em>{}</em>".format(self.word[3:-2])
-        else:
-            self.word = "<em>{}</em>".format(self.word[1:-1])
+    def check_for_aside_in_lines(self):
+        lines = ''
+        for line in self.lines:
+            if line and line[0] == '{':
+                if '|' in line:
+                    if '}' in line:
+                        center_index = line.index('|')
+                        right_parenthesis_index = line.index('}')
+                        if center_index < right_parenthesis_index:
+                            aside_type = line[1:center_index]
+                            aside_title = line[center_index + 1:right_parenthesis_index]
+                            line = f'<aside cat=”{aside_type}”><header>{aside_title}</header>' \
+                                f'<main> {line[right_parenthesis_index+1:]} </main></aside>'
+                        else:
+                            sys.exit(f'Niepoprawnie domknięte znaki!')
+                    else:
+                        sys.exit(f'Niepoprawnie domknięte znaki!')
+                else:
+                    sys.exit(f'Niepoprawnie domknięte znaki!')
+            lines += line + '\n'
+        self.lines = lines
+        self.split_for_lines(self.lines[:-1])
 
-    def two_stars(self):
-        if self.check_heading():
-            self.word = "#<strong>{}</strong>".format(self.word[3:-2])
-        else:
-            self.word = "<strong>{}</strong>".format(self.word[2:-2])
+    def check_for_href(self):
+        lines = ''
+        for line in self.lines:
+            if '[' in line:
+                if '|' in line:
+                    if ']' in line:
+                        left_parenthesis_index = line.index('[')
+                        center_index = line.index('|')
+                        right_parenthesis_index = line.index(']')
+                        if center_index < right_parenthesis_index:
+                            href_address = line[left_parenthesis_index+1:center_index]
+                            href_text = line[center_index + 1:right_parenthesis_index]
+                            href = f'<a href=”{href_address}”>{href_text}</a>'
+                            line = line[:left_parenthesis_index] + href + line[right_parenthesis_index+1:]
+                    else:
+                        sys.exit(f'Niepoprawnie domknięte znaki!')
+                else:
+                    sys.exit(f'Niepoprawnie domknięte znaki!')
+            lines += line + '\n'
+        self.lines = lines
+        self.split_for_lines(self.lines[:-1])
 
-    def underscore_exclamation(self):
-        if self.check_heading():
-            self.word = "#<ins>{}</ins>".format(self.word[3:-2])
-        else:
-            self.word = "<ins>{}</ins>".format(self.word[2:-2])
-
-    def dash_exclamation(self):
-        if self.check_heading():
-            self.word = "#<del>{}</del>".format(self.word[3:-2])
-        else:
-            self.word = "<del>{}</del>".format(self.word[2:-2])
-
-    def check_url_text(self):
-        if "[" in self.word and "|" in self.word and "]" in self.word:
-            return True
-
-    def paragraph(self):
-        if self.check_heading():
-            pass
-        else:
-            self.word = "<p>{}</p>".format(self.word[0:])
-
-    def link_text(self):
-        while "[" in self.line and "|" in self.line:
-            if "]" in self.line:
-                left_parenthesis_index = self.line.index("[")
-                line_index = self.line.index("|")
-                right_parenthesis_index = self.line.index("]")
-                link = self.line[left_parenthesis_index + 1:line_index]
-                text = self.line[line_index + 1:right_parenthesis_index]
-                self.line = self.line[0:left_parenthesis_index] + '<a href="{}">{}</a>'.format(link, text) + \
-                    self.line[right_parenthesis_index+1:]
+    def split(self):
+        lines = []
+        for line in self.lines:
+            if line[0:6] == '<aside':
+                index_aside = line.index('<main>')
+                aside = line[0:index_aside+6]
+                line = line[index_aside+7:]
+                line = line.split(' ')
+                line.insert(0, aside)
+                lines.append(line)
+            elif line[0:3] == '<h1':
+                index_h1 = line.index('<h1')
+                h1 = line[0:index_h1+12]
+                line = line[index_h1+13:]
+                line = line.split(' ')
+                line.insert(0, h1)
+                lines.append(line)
             else:
-                sys.exit(f'Niepoprawnie domknięte znaki! ---> [link|text] Wyszukaj błąd ręcznie.')
+                line = line.split(' ')
+                lines.append(line)
+        self.lines = lines
 
-    def check_line(self):
-        if '\n' in self.text:
-            self.text = self.text.split('\n')
+    def check_for_double_char(self, char_left, char_right, html):
+        lines = ''
+        for word in self.lines:
+            if char_left in word:
+                if char_right in word:
+                    word = word.replace(char_left, f'<{html}>')
+                    word = word.replace(char_right, f'</{html}>')
+            lines += word + '\n'
+        self.lines = lines
+        self.split_for_lines(self.lines[:-1])
 
-    def delete_first_line(self):
-        self.output = self.output.split('\n')[1:]
+    def check_for_single_char(self, char_left, char_right, html):
+        lines = ''
+        for word in self.lines:
+            if char_left == word[0]:
+                if char_right in word[-1]:
+                    word = word.replace(char_left, f'<{html}>')
+                    word = word.replace(char_right, f'</{html}>')
+            lines += word + '\n'
+        self.lines = lines
+        self.split_for_lines(self.lines[:-1])
 
-    def check_heading(self):
-        if self.word[0] == "#":
-            return True
+    def split_two_arrays(self):
+        lines = []
+        for line in self.lines:
+            for word in line:
+                lines.append(word)
+        self.lines = lines
 
-    def heading(self, heading_id):
-        self.line = self.line[1:]
-        self.line = self.line.replace(" ", "\n")
-        self.line = "<h1 id=”{}”>{}</h1>".format(heading_id, self.line)
-
-    def output_line(self):
-        heading_id = 0
-        for self.line in self.output:
-            # Removing space as the last character in line.
-            self.line = self.line[:-1]
-            if self.line[0] == "#":
-                self.heading(heading_id)
-                heading_id += 1
-            self.link_text()
-            self.final_output += self.line
-            self.final_output += "\n"
+    def add_parentheses(self):
+        lines = []
+        for word in self.lines:
+            if word.isalnum():
+                word = f'<p>{word}</p>'
+            lines.append(word)
+        self.lines = lines
 
     def print(self):
-        print(self.final_output)
+        self.split_for_lines(self.text)
 
-    def characters(self):
-        for self.line in self.text:
-            self.line = self.line.split()
-            self.output += "\n"
-            for self.word in self.line:
-                if ">>" in self.word:
-                    if '<<' in self.word:
-                        index_one = self.word.index('>>')
-                        index_two = self.word.index('<<')
-                        if index_one < index_two:
-                            self.sharp_parentheses()
-                        else:
-                            sys.exit(f'Niepoprawnie domknięte znaki! ---> {self.word}')
-                    else:
-                        sys.exit(f'Niepoprawnie domknięte znaki! ---> {self.word}')
-                elif self.word[0] == "*" and self.word[1] != "*":
-                    if self.word[-1] == "*" and self.word[-2] != "*":
-                        index_one = self.word.index('*')
-                        index_two = self.word.index('*', index_one+1)
-                        if index_one < index_two:
-                            self.one_star()
-                        else:
-                            sys.exit(f'Niepoprawnie domknięte znaki! ---> {self.word}')
-                    else:
-                        sys.exit(f'Niepoprawnie domknięte znaki! ---> {self.word}')
-
-                elif self.word[0] == "*" and self.word[1] == "*":
-                    if self.word[-1] == "*" and self.word[-2] == "*":
-                        index_one = self.word.index('*')
-                        index_two = self.word.index('*', index_one + 2)
-                        if index_one < index_two:
-                            self.two_stars()
-                        else:
-                            sys.exit(f'Niepoprawnie domknięte znaki! ---> {self.word}')
-                    else:
-                        sys.exit(f'Niepoprawnie domknięte znaki! ---> {self.word}')
-
-                elif self.word[0] == "_" and self.word[1] == "!":
-                    if self.word[-1] == "_":
-                        if self.word[-2] == "!":
-                            index_one = self.word.index('!')
-                            index_two = self.word.index('!', index_one + 1)
-                            if index_one < index_two:
-                                self.underscore_exclamation()
-                            else:
-                                sys.exit(f'Niepoprawnie domknięte znaki! ---> {self.word}')
-                        else:
-                            sys.exit(f'Niepoprawnie domknięte znaki! ---> {self.word}')
-                    else:
-                        sys.exit(f'Niepoprawnie domknięte znaki! ---> {self.word}')
-
-                elif self.word[0] == "-" and self.word[1] == "!":
-                    if self.word[-1] == "-" and self.word[-2] == "!":
-                        index_one = self.word.index('!')
-                        index_two = self.word.index('!', index_one + 1)
-                        if index_one < index_two:
-                            self.dash_exclamation()
-                        else:
-                            sys.exit(f'Niepoprawnie domknięte znaki! ---> {self.word}')
-                    else:
-                        sys.exit(f'Niepoprawnie domknięte znaki! ---> {self.word}')
-                elif self.word[0] != "[":
-                    self.paragraph()
-                self.output += self.word + " "
+        self.check_for_headings_in_lines()
+        self.check_for_aside_in_lines()
+        self.split()
+        self.split_two_arrays()
+        while self.href_check:
+            for line in self.lines:
+                if '[' in line:
+                    pass
+                else:
+                    self.href_check = False
+                self.check_for_href()
+        self.check_for_double_char('>>', '<<', 'q')
+        self.check_for_double_char('**', '**', 'strong')
+        self.check_for_single_char('*', '*', 'em')
+        self.check_for_double_char('_!', '!_', 'ins')
+        self.check_for_double_char('-!', '!-', 'del')
+        self.add_parentheses()
+        for word in self.lines:
+            print(word)
 
 
 if __name__ == "__main__":
-    generator = HTMLGenerator("#>>XD<< XD W **Ddddd** *[link|text]* **WWw** _!pwoep!_ -!safasf!- dfsg >>[link|text]<<\n"
-                              "#xdddddxd [link|text]")
-    generator.check_line()
-    generator.characters()
-    generator.delete_first_line()
-    generator.output_line()
+    generator = HTMLGenerator("{safa|sf}>>XD<< >>XD<< W **Ddddd** *[link|text]* **WWw** _!pwoep!_ -!safasf!- dfsg **[link|text]\n"
+                              "#xdddddxd [link|text]\n#xddddddd")
+
     generator.print()
