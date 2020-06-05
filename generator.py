10a1aa1505e421acf650086d2af6b24dd2b354ac
szypkiwonsz <kacpersawicki321@gmail.com>
2020-06-05 21:54:29 +0200

    Translation to english

--- generator.py
+++ generator.py
@@ -17,8 +17,8 @@ class HTMLGenerator:
                 try:
                     open_char_index = line.index(open_char)
                     close_char_index = line.index(close_char, open_char_index + 2)
-                    # Dodajemy znak '$' przed i po każdym nowym znaczniku,
-                    # aby nasze znaki nie połączyły się ze znacznikami html.
+                    # Adding '$' before and after every new marker,
+                    # so that our characters do not connect with the html tags.
                     if open_char == single_char:
                         self.lines[i] = f'{line[:open_char_index]}${new_open_char}$' \
                             f'{line[open_char_index + 1:close_char_index]}${new_close_char}$' \
@@ -29,7 +29,7 @@ class HTMLGenerator:
                             f'{line[close_char_index + 2:]}'
                 except ValueError:
                     check = False
-            # Usuwamy wszystkie znaki dolara z linii.
+            # Removing all dollar signs from the line.
             self.lines[i] = self.lines[i].replace('$', '')
 
     def check_heading(self):
@@ -49,7 +49,7 @@ class HTMLGenerator:
                     self.lines[i] = f'<aside cat="{aside_type}"><header>{header_title}</header><main>{main_text}' \
                         f'</main></aside>'
                 else:
-                    sys.exit(f'Niepoprawnie utworzony nagłówek w linii ---> {self.raw_lines[i]}')
+                    sys.exit(f'Incorrectly created header in a line ---> {self.raw_lines[i]}')
             else:
                 self.lines[i] = f'<p>{line}</p>'
 
@@ -68,7 +68,7 @@ class HTMLGenerator:
                         self.lines[i] = f'{line[:index_left_parenthesis]}<a href="{href_address}">{href_text}</a>' \
                             f'{line[index_right_parenthesis + 1:]}'
                     else:
-                        sys.exit(f'Niepoprawnie utworzony atrybut "<a href" w linii ---> {self.raw_lines[i]}')
+                        sys.exit(f'Incorrectly created attribute "<a href" in a line ---> {self.raw_lines[i]}')
                 except ValueError:
                     check = False
 
@@ -92,35 +92,35 @@ class HTMLGenerator:
         self.check_chars('_!', '!_', '<ins>', '</ins>')
         self.check_chars('-!', '!-', '<del>', '</del>')
         self.check_correct_characters(['>>', '<<', '*', '**', '-!', '!-', '_!', '!_'],
-                                      'Niepoprawnie domknięte znaki w linii')
+                                      'Incorrectly closed characters in a line.')
         try:
             self.check_heading()
         except IndexError:
-            print('Usuń puste linie z pliku!')
+            print('Delete empty lines from file!')
             sys.exit()
 
-        self.check_correct_characters(['#', '{', '}'], 'Nie możesz tworzyć więcej niż jednego nagłówka w linii lub'
-                                                       'nagłówek został niepoprawnie stworzony.')
+        self.check_correct_characters(['#', '{', '}'], 'You cannot create more than one header per line or the'
+                                                       'header was created incorrectly.')
         self.check_href()
-        self.check_correct_characters(['[', ']'], 'Niepoprawnie utworzony atrybut "<a href" w linii')
+        self.check_correct_characters(['[', ']'], 'Incorrectly created attribute "<a href" in a line.')
         for line in self.lines:
             text += line + '\n'
         self.write_to_file('output.html', text)
-        print('---> Poprawnie zapisano dane do pliku output.html')
+        print('---> The data was correctly saved to the output.html file')
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--file_name", help="Wybiera plik z którego pobiera tekst.")
+    parser.add_argument("--file_name", help="Selects the file from which it retrieves the text.")
 
     args = parser.parse_args()
     if not any(vars(args).values()):
-        print("Nie podałeś nazwy pliku!")
+        print("You didn't enter a file name!")
     elif args.file_name:
         try:
             input_text = open(args.file_name).read()
             generator = HTMLGenerator(input_text)
             generator.output()
         except FileNotFoundError as e:
-            print('Nieprawidłowa nazwa pliku. Pamiętaj aby dodać rozszerzenie do nazwy pliku.')
+            print('Invalid file name. Remember to add the extension to the file name.')
